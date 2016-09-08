5ee7fa28051377c63f0a936b086d82b62e99b429
Matias Korhonen <matias@kiskolabs.com>
2016-09-08 20:55:56 +0300

    Simple loop

--- bmp180reporter.go
+++ bmp180reporter.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/kidoman/embd"
 	_ "github.com/kidoman/embd/host/rpi"
@@ -13,27 +14,35 @@ func init() {}
 func main() {
 	bus := embd.NewI2CBus(1)
 	sensor := bmp180.New(bus)
-	pressure, err := sensor.Pressure()
 
-	if err != nil {
-		log.Fatal(err)
-	} else {
-		log.Printf("Pressure %v", pressure)
-	}
+	sensor.Run()
+	defer sensor.Close()
 
-	altitude, err := sensor.Altitude()
+	for {
+		pressure, err := sensor.Pressure()
 
-	if err != nil {
-		log.Fatal(err)
-	} else {
-		log.Printf("Altitude %v", altitude)
-	}
+		if err != nil {
+			log.Fatal(err)
+		} else {
+			log.Printf("Pressure %v", pressure)
+		}
 
-	temperature, err := sensor.Temperature()
+		altitude, err := sensor.Altitude()
 
-	if err != nil {
-		log.Fatal(err)
-	} else {
-		log.Printf("Temperature %v", temperature)
+		if err != nil {
+			log.Fatal(err)
+		} else {
+			log.Printf("Altitude %v", altitude)
+		}
+
+		temperature, err := sensor.Temperature()
+
+		if err != nil {
+			log.Fatal(err)
+		} else {
+			log.Printf("Temperature %v", temperature)
+		}
+
+		time.Sleep(5 * time.Second)
 	}
 }
